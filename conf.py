364eb1df2bdb4595169b22b656041f5c19cd66b8
Eduardo Vasconcelos <107057997+EduardoVasconceloss@users.noreply.github.com>
2023-05-12 10:26:45 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,6 +3,14 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('/home/eduardo/rtd_docs/'))
+
+import rtd_docs.home
+import rtd_docs.index
+import rtd_docs.instalacao
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +22,16 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = [
+  'myst_parser',
+  'sphinx.ext.autosummary',
+  'sphinx.ext.autodoc',
+  'sphinx.ext.mathjax',
+  'sphinx.ext.viewcode',
+  'sphinx.ext.napoleon',
+  'sphinx.ext.intersphinx',
+  'sphinx.ext.extlinks',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
